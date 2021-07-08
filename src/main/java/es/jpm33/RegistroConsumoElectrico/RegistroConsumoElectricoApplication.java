3d72543337b6ebcaf668ad28ed37748f6f6fc7a9
Jose PEREZ <j.perezm@umh.es>
2021-07-08 11:39:04 +0200

    Añado .gitignore e inicio proyecto springboot

---
+++ src/main/java/es/jpm33/RegistroConsumoElectrico/RegistroConsumoElectricoApplication.java
@@ -0,0 +1,13 @@
+package es.jpm33.RegistroConsumoElectrico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroConsumoElectricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroConsumoElectricoApplication.class, args);
+	}
+
+}
